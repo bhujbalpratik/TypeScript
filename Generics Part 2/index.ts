a8210dc9b225ebbe2dae4a8c9b91d18fa3c507b9
bhujbal pratik <pratikbhujbal743@gmail.com>
2024-05-01 22:09:34 +0530

    Generics in Array and Arrow function added

--- Generics Part 2/index.ts
+++ Generics Part 2/index.ts
@@ -1,15 +1,34 @@
-const myfunc1 = (val: string | number): string | number => {
-  return val
-}
-const myfunc2 = (val: any): any => {
-  return "nothing"
-}
-const myfunc3 = <Type>(val: Type): Type => {
-  return val
+// const myfunc1 = (val: string | number): string | number => {
+//   return val
+// }
+// const myfunc2 = (val: any): any => {
+//   return "nothing"
+// }
+// const myfunc3 = <Type>(val: Type): Type => {
+//   return val
+// }
+
+// const myfunc4 = <T>(val: T): T => {
+//   return val
+// }
+
+// myfunc4(33)
+
+// ***************************************Genrics in Array and Arrow Function**********************************************
+
+interface Bootle {
+  brand: string
+  type: number
 }
 
-const myfunc4 = <T>(val: T): T => {
-  return val
+function getSearchProducts<T>(products: T[]): T {
+  // do some database operations
+  const myIndex = 3
+  return products[myIndex]
 }
 
-myfunc4(33)
+const getMoreSearchProducts = <T>(products: T[]): T => {
+  // do some database operations
+  const myIndex = 4
+  return products[myIndex]
+}
